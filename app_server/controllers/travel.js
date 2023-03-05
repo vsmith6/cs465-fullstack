39be3359be013a92e8f1c725c134ddfd16d945ff
vsmith6 <Valerie.Smith@walmart.com>
2023-03-05 15:24:46 -0500

    adding module 2 assignment files

---
+++ app_server/controllers/travel.js
@@ -0,0 +1,11 @@
+/* GET travel view */
+const travel = (req,res) => {
+    pageTitle = process.env.npm_package_description + 
+       "  - Travel";
+    res.render('travel', { title: pageTitle});
+};
+
+module.exports = {
+    travel
+    
+};
